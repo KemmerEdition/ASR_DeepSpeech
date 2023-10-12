5c7e6ca2eed60bbe70eb6f4b2fa9b07c10045209
Anastasia Kemmer <anastasiya_kemmer@mail.ru>
2023-10-13 01:39:06 +0300

    config file

--- hw_asr/model/__init__.py
+++ hw_asr/model/__init__.py
@@ -1,5 +1,6 @@
 from hw_asr.model.baseline_model import BaselineModel
 from hw_asr.model.deep_speech_LSTM import DeepSpeech
+from hw_asr.model.deep_speech_GRU import DeepSpeech
 
 __all__ = [
     "BaselineModel",
