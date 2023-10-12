0641abbe1aba0ea56522874f52043ada99750da3
Anastasia Kemmer <anastasiya_kemmer@mail.ru>
2023-10-13 01:28:18 +0300

    add deepspeech gru

--- hw_asr/model/__init__.py
+++ hw_asr/model/__init__.py
@@ -1,5 +1,5 @@
 from hw_asr.model.baseline_model import BaselineModel
-from hw_asr.model.deep_speech_based import DeepSpeech
+from hw_asr.model.deep_speech_LSTM import DeepSpeech
 
 __all__ = [
     "BaselineModel",
