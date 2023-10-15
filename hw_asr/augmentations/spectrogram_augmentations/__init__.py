106d7430920a4e786b1bf3860b5fc1dddedb4185
Anastasia Kemmer <anastasiya_kemmer@mail.ru>
2023-10-15 14:56:54 +0300

    fix aug

--- hw_asr/augmentations/spectrogram_augmentations/__init__.py
+++ hw_asr/augmentations/spectrogram_augmentations/__init__.py
@@ -1,9 +1,9 @@
 from hw_asr.augmentations.spectrogram_augmentations.FrequencyMasking import FrequencyMasking
 from hw_asr.augmentations.spectrogram_augmentations.TimeMasking import TimeMasking
-from hw_asr.augmentations.spectrogram_augmentations.TimeStretch import TimeStretch
+# from hw_asr.augmentations.spectrogram_augmentations.TimeStretch import TimeStretch
 
 __all__ = [
     "FrequencyMasking",
     "TimeMasking",
-    "TimeStretch"
+
 ]
