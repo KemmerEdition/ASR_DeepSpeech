6f0afb1ff010cd88e8df665759019b7fb2f9633d
Anastasia Kemmer <anastasiya_kemmer@mail.ru>
2023-10-17 13:56:05 +0300

    add new version of ds

--- hw_asr/model/__init__.py
+++ hw_asr/model/__init__.py
@@ -3,10 +3,12 @@ from hw_asr.model.deep_speech_LSTM import DeepSpeechLSTM
 from hw_asr.model.deep_speech_GRU import DeepSpeechGRU
 from hw_asr.model.deep_speech_rnn import DeepSpeechRNN
 from hw_asr.model.deep_speech_2 import DeepSpeech2
+from hw_asr.model.deep_speech_GRU_5_32 import DeepSpeechGRU_BIG
 __all__ = [
     "BaselineModel",
     "DeepSpeechGRU",
     "DeepSpeechLSTM",
     "DeepSpeechRNN",
-    "DeepSpeech2"
+    "DeepSpeech2",
+    "DeepSpeechGRU_BIG"
 ]
